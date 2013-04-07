2457e9555e27c120b93104c75a2df6f6c7db38d6
Avinash Kundaliya <avinash@avinash.com.np>
2013-04-07 12:45:50 +0545

    add category pages

--- _plugins/cat_and_tag_generator.rb
+++ _plugins/cat_and_tag_generator.rb
@@ -17,7 +17,7 @@ module Jekyll
     def build_subpages(site, type, posts) 
       posts[1] = posts[1].sort_by { |p| -p.date.to_f }     
       atomize(site, type, posts)
-      #paginate(site, type, posts)
+      paginate(site, type, posts)
     end
 
     def atomize(site, type, posts)
