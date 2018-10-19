afb011be4a9e69dadb8ead70a13430758dc31c4f
Avinash <avinash@avinash.com.np>
2018-10-19 18:47:48 +0545

    webmentions

---
+++ themes/avinash/static/main.js
@@ -0,0 +1,29 @@
+(function() {
+
+  const commentBox = document.getElementById("webmentions");
+
+  if(!commentBox)
+    return
+
+  const { url } = commentBox.dataset
+  fetch(`https://webmention.io/api/mentions?target=${url}`)
+  .then(r => r.json())
+  .then(r => {
+    console.log(r.links)
+    return r.links.map(el => {
+      if(el.data.author && el.activity.sentence_html)
+        return `  <div class="mentionSingle">
+            <div class="mention">
+              ${el.activity.sentence_html}
+              ${ el.data.published  ?
+              `<div class="meta"><a href="${el.data.url}">${new Date(el.data.published_ts * 1000).toLocaleDateString('se')}</a></div>` :
+              ""}
+            </div>
+          </div>
+        `;
+    }).join("")
+  })
+  .then(res => {
+    commentBox.innerHTML = res
+  })
+}())
