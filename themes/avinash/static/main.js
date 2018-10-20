0c9d513d3bae68bd7477ddd568e4e6d6f5fb790a
Avinash <avinash@avinash.com.np>
2018-10-20 16:23:54 +0545

    webmention more

--- themes/avinash/static/main.js
+++ themes/avinash/static/main.js
@@ -2,27 +2,53 @@
 
   const commentBox = document.getElementById("webmentions");
 
-  if(!commentBox)
-    return
+  if(!commentBox) return
 
   const { url } = commentBox.dataset
+
   fetch(`https://webmention.io/api/mentions?target=${url}`)
   .then(r => r.json())
-  .then(r => {
-    return r.links.map(el => {
-      if(el.data.author && el.activity.sentence_html)
-        return `  <div class="mentionSingle">
-            <div class="mention">
-              ${el.activity.sentence_html}
-              ${ el.data.published  ?
-              `<div class="meta"><a href="${el.data.url}">${new Date(el.data.published_ts * 1000).toLocaleDateString('se')}</a></div>` :
-              ""}
-            </div>
-          </div>
-        `;
-    }).join("")
-  })
-  .then(res => {
-    commentBox.innerHTML = res
-  })
+  .then(r => r.links.map(processMention).join(""))
+  .then(res => commentBox.innerHTML = res)
+
 }())
+
+function processMention(mention) {
+  const { content, author, published_ts, url} = mention.data
+
+  if(!author) return
+
+  if(!content && !mention.activity.sentence_html) return
+
+  const published_date = published_ts ?  `<span class="meta"><a href="${url}">${new Date(published_ts * 1000).toLocaleDateString('se')}</a></span>` : '';
+
+  switch(mention.activity.type){
+    case "repost":
+      return `<div class="mentionSingle">
+        <div class="mention">
+          ${ published_date }
+          <a href="${author.url}">${author.name}</a> retweeted <a href="${url}">a tweet</a>
+        </div>
+      </div>`;
+
+    case "reply":
+      return `<div class="mentionSingle">
+        <div class="mention">
+          ${ published_date }
+          <a href="${author.url}">${author.name}</a> replied ${content} <a href="${url}">on twitter</a>
+        </div>
+      </div>`
+
+    case "like":
+      return `<div class="mentionSingle">
+        <div class="mention">
+          ${ published_date }
+          <a href="${author.url}">${author.name}</a> liked <a href="${url}">a tweet</a>
+        </div>
+      </div>`;
+
+    default:
+      return "";
+  }
+
+}
